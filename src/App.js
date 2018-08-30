dee62c8e6f0c3d3c23ff7c2554ad0cd40111da40
Ashish kumar shrivastav <a.shrivastav@globallogic.com>
2018-08-30 23:07:59 +0530

    Pure components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import logo from './logo.svg';
 import FunctionalComponent from './FunctionalComponent';
 import ClassComponent from './ClassComponent';
@@ -18,12 +18,35 @@ import Fragments from './Fragments';
 
 
 
+/*
+  - In react, when you call setState method, re-rendering will happen and its kind of blind as it doesn't
+    look that if you have really changed the value or not. And sometimes these updates are not needed.
+    We can fix it using Pure components.
+*/
 
-class App extends Component {
-  numberList = [1,2,3,4];
+function Temp(props) {
+  console.log('Rendering myself');
+  return <div>{props.val}</div>
+}
+class App extends PureComponent {
+  state = {
+    val: 1
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState(() => {
+        return { val: 1 }; //If the value is same, component should not update
+        //return { val: Math.random() }
+      });
+    }, 2000);
+  }
+  // One way to stop rendering for the same value is to use shouldComponentLifecycle lifecycle hook
+  // shouldComponentUpdate(nextProp, nextState) {
+  //   return !(this.state.val === nextState.val);
+  // }
   render() {
     return (
-      <Fragments />
+      <Temp val={this.state.val}/>
     );
 
   }
